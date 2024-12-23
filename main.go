e65f01ba02cbb580803cfc2f6c326ea0f02d7801
Sam Maton <sam.maton91@gmail.com>
2024-12-23 09:07:56 +0000

    added confirm password validation for signup page

--- main.go
+++ main.go
@@ -13,28 +13,36 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// http.ServeFile(w, r, "index.html")
 		tmpl, err := template.New("").ParseFiles("index.html", "./layouts/main-layout.html")
 		if err != nil {
 			fmt.Println(err)
 		}
-		// check your err
-		err = tmpl.ExecuteTemplate(w, "main-layout", nil)
 
+		err = tmpl.ExecuteTemplate(w, "main-layout", nil)
 		if err != nil {
 			fmt.Println(err)
 		}
 	})
 
 	mux.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		// http.ServeFile(w, r, "./pages/login.html")
 		tmpl, err := template.New("").ParseFiles("./pages/login.html", "./layouts/main-layout.html")
 		if err != nil {
 			fmt.Println(err)
 		}
-		// check your err
-		err = tmpl.ExecuteTemplate(w, "main-layout", nil)
 
+		err = tmpl.ExecuteTemplate(w, "main-layout", nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	mux.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.New("").ParseFiles("./pages/sign-up.html", "./layouts/main-layout.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		err = tmpl.ExecuteTemplate(w, "main-layout", nil)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -48,8 +56,42 @@ func main() {
 		fmt.Println(password)
 	})
 
-	mux.HandleFunc("GET /api/clicked", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "<h1>HTMX is awesome!</h1>")
+	mux.HandleFunc("POST /api/validate-password", func(w http.ResponseWriter, r *http.Request) {
+		password := r.FormValue("signup-password")
+		confirm := r.FormValue("signup-password-confirm")
+
+		if password == confirm {
+			io.WriteString(w, fmt.Sprintf(`
+				<div hx-target="this" hx-swap="outerHTML">
+					<label for="signup-password-confirm">Confirm Password:</label>
+					<input
+						class="valid-password"
+						type="password"
+						id="signup-password-confirm"
+						name="signup-password-confirm"
+						required
+						value="%s"
+						hx-post="/api/validate-password"
+					/>
+				</div>
+			`, confirm))
+		} else {
+			io.WriteString(w, fmt.Sprintf(`
+				<div hx-target="this" hx-swap="outerHTML">
+					<label for="signup-password-confirm">Confirm Password:</label>
+					<input
+						class="invalid-password"
+						type="password"
+						id="signup-password-confirm"
+						name="signup-password-confirm"
+						required
+						value="%s"
+						hx-post="/api/validate-password"
+					/>
+					<p class="password-message--error">The two passwords do not match.</p>
+				</div>
+			`, confirm))
+		}
 	})
 
 	log.Println("Serving app on http://localhost:4321/")
