73b44336349c109dba4ef002d1d02d4a02fe4552
Sam Maton <sam.maton91@gmail.com>
2025-01-07 17:29:39 +0000

    added methods to create and validate JWT

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.23.2
 
 require github.com/joho/godotenv v1.5.1
 
+require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+
 require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,17 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type CustomClaims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
 
 func HashPassword(password string) (string, error) {
 	p, err := bcrypt.GenerateFromPassword([]byte(password), 10)
@@ -12,4 +23,43 @@ func CheckPasswordHash(password, hash string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 
 	return err
+}
+
+func MakeJWT(userID uuid.UUID, username, tokenSecret string) (string, error) {
+	claims := CustomClaims{
+		username,
+		jwt.RegisteredClaims{
+			Issuer:    "htmxTodos",
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(time.Hour)),
+			Subject:   userID.String(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := CustomClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	tokenUserID, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userId, err := uuid.Parse(tokenUserID)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userId, nil
 }
