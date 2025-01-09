2a2dcd1b0ffb79e3fb9412038d7d9999ef1ddcd3
Sam Maton <sam.maton91@gmail.com>
2025-01-09 13:38:50 +0000

    added queries for todos

---
+++ sql/queries/todos.sql
@@ -0,0 +1,9 @@
+-- name: GetTodosByUserId :many
+SELECT id, created_at, updated_at, description, title, completed
+FROM todos
+WHERE user_id = $1;
+
+-- name: CreateTodo :one
+INSERT INTO todos(id, created_at, updated_at, user_id, description, title, completed)
+VALUES (gen_random_uuid(), NOW(), NOW(), $1, $2, $3, $4)
+RETURNING id, created_at, updated_at, description, title, completed;
